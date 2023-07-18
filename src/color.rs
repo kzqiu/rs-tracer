ed55a9eab7de2d6c5289b65cba8fe24405bf9958
Kevin Qiu <kzqiu01@gmail.com>
2023-07-18 01:33:37 -0400

    Implemented anti-aliasing for ray traced objects by generating random samples of color for each pixel.

--- src/color.rs
+++ src/color.rs
@@ -1,11 +1,27 @@
-use crate::vec3::Vec3;
-use image::Rgb;
+use crate::hittable::{HitRecord, Hittable};
+use crate::ray::Ray;
+use crate::vec3::{unit_vector, Vec3};
 
-pub fn write_color(pixel: Rgb<u8>, c: Vec3) {
-    println!(
-        "{} {} {}",
-        (255.999 * c.x()) as i32,
-        (255.999 * c.y()) as i32,
-        (255.999 * c.z()) as i32
-    );
+pub fn ray_color(r: &Ray, world: &impl Hittable) -> Vec3 {
+    let mut rec = HitRecord::new();
+
+    if world.hit(r, 0., crate::INF, &mut rec) {
+        return 0.5 * (rec.norm + Vec3::new(1., 1., 1.));
+    }
+
+    let unit_dir = unit_vector(r.dir);
+    let t = 0.5 * (unit_dir.y() + 1.);
+    (1. - t) * Vec3::new(1., 1., 1.) + t * Vec3::new(0.5, 0.7, 1.)
+}
+
+pub fn clamp(x: f64, min: f64, max: f64) -> f64 {
+    if x < min {
+        return min;
+    }
+
+    if x > max {
+        return max;
+    }
+
+    x
 }
--- src/main.rs
+++ src/main.rs
@@ -9,11 +9,11 @@ mod sphere;
 mod vec3;
 
 use crate::camera::Camera;
-use crate::hittable::{HitRecord, Hittable};
+use crate::color::{clamp, ray_color};
 use crate::hittable_list::HittableList;
-use crate::ray::Ray;
 use crate::sphere::Sphere;
-use crate::vec3::{unit_vector, Vec3};
+use crate::vec3::Vec3;
+use rand::Rng;
 use rayon::iter::{IndexedParallelIterator, ParallelIterator};
 use rayon::prelude::ParallelSliceMut;
 use std::ops::DerefMut;
@@ -29,23 +29,12 @@ fn deg_to_rad(deg: f64) -> f64 {
     return deg * PI / 180.;
 }
 
-fn ray_color(r: &Ray, world: &impl Hittable) -> Vec3 {
-    let mut rec = HitRecord::new();
-
-    if world.hit(r, 0., INF, &mut rec) {
-        return 0.5 * (rec.norm + Vec3::new(1., 1., 1.));
-    }
-
-    let unit_dir = unit_vector(r.dir);
-    let t = 0.5 * (unit_dir.y() + 1.);
-    (1. - t) * Vec3::new(1., 1., 1.) + t * Vec3::new(0.5, 0.7, 1.)
-}
-
 fn main() {
     // Image
     const ASPECT_RATIO: f64 = 16. / 9.;
-    const WIDTH: u32 = 400;
+    const WIDTH: u32 = 800;
     const HEIGHT: u32 = (WIDTH as f64 / ASPECT_RATIO) as u32;
+    const SAMPLES_PER_PIXEL: u32 = 100;
 
     let mut img = RgbImage::new(WIDTH, HEIGHT);
 
@@ -63,20 +52,28 @@ fn main() {
         .enumerate()
         .for_each(|(y, row)| {
             for (x, pixel) in row.chunks_exact_mut(3).enumerate() {
-                let u: f64 = (x as f64) / (WIDTH as f64 - 1.);
-                let v: f64 = ((HEIGHT - y as u32) as f64) / (HEIGHT as f64 - 1.);
-                let r = camera.get_ray(u, v);
-
                 if x >= WIDTH as usize || y >= HEIGHT as usize {
                     continue;
                 }
 
-                let color = ray_color(&r, &world);
-                let Rgb(a) = Rgb([
-                    (255.99 * color.x()) as u8,
-                    (255.99 * color.y()) as u8,
-                    (255.99 * color.z()) as u8,
-                ]);
+                let mut color = Vec3::new(0., 0., 0.);
+
+                for s in 0..SAMPLES_PER_PIXEL {
+                    let u: f64 = (x as f64 + rand::thread_rng().gen::<f64>()) / (WIDTH as f64 - 1.);
+                    let v: f64 = ((HEIGHT - y as u32) as f64 + rand::thread_rng().gen::<f64>())
+                        / (HEIGHT as f64 - 1.);
+
+                    let r = camera.get_ray(u, v);
+                    color += ray_color(&r, &world);
+                }
+
+                let scale = 1. / SAMPLES_PER_PIXEL as f64;
+
+                let a = clamp(color.x() * scale, 0., 0.999);
+                let b = clamp(color.y() * scale, 0., 0.999);
+                let c = clamp(color.z() * scale, 0., 0.999);
+
+                let Rgb(a) = Rgb([(256. * a) as u8, (256. * b) as u8, (256. * c) as u8]);
 
                 pixel.copy_from_slice(&a);
             }
